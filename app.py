b4c746f54185f6430fead2dd0ff17eb488a2e0da
rabinliu-boop <rabin.liu@gmail.com>
2026-04-15 03:00:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,41 @@
 from flask import Flask, request
+import requests
 
 app = Flask(__name__)
 
-# 測試首頁（避免404）
+CHANNEL_ACCESS_TOKEN = "你的token"
+
 @app.route("/")
 def home():
     return "Bot is running"
 
-# LINE webhook
 @app.route("/callback", methods=["POST"])
 def callback():
+    data = request.json
     print("🔥 收到LINE請求")
-    print(request.json)
+    print(data)
+
+    for event in data["events"]:
+        if event["type"] == "message":
+            text = event["message"]["text"]
+            reply_token = event["replyToken"]
+
+            reply_message(reply_token, "你說的是：" + text)
+
     return "OK"
+
+def reply_message(reply_token, text):
+    url = "https://api.line.me/v2/bot/message/reply"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {CHANNEL_ACCESS_TOKEN}"
+    }
+    payload = {
+        "replyToken": replyToken,
+        "messages": [{"type": "text", "text": text}]
+    }
+
+    requests.post(url, headers=headers, json=payload)
 
 if __name__ == "__main__":
     app.run()
